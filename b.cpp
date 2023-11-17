33883ca3235f585c76a4bf9fcc8d56a574fa5b26
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-17 10:30:50 +0600

    created file for testing purpose

---
+++ b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << "Finally Fixed " << x << endl;
+
+    int i = 0;
+
+    for (i = 0; i < 1000000; i++)
+    {
+        // cout << i << endl;
+    }
+
+    cout << i << endl;
+}
---
+++ tmp.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << x << endl;
+    cout << "Hello Workld" << endl;
+}
